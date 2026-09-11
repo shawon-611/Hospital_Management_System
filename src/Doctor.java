85520fcf17735815fcae129510b06226a7555a31
MD Alauddin Shawon <sa9881950@gmail.com>
2026-09-11 21:42:14 +0600

    Doctor details add

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,3 +1,15 @@
 public class Doctor extends Person{
 
+    private String specialization;
+    private String qualification;
+    private int salary;
+
+    Doctor(String name, int id, String phone_number, String address, String specialization, String qualification, int salary)
+    {
+        super(name, id, phone_number, address);
+        this.specialization = specialization;
+        this.qualification = qualification;
+        this.salary = salary;
+    }
+
 }
