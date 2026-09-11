dda631de633b5729512384f6a8abb0879cd355c9
MD Alauddin Shawon <sa9881950@gmail.com>
2026-09-11 20:10:53 +0600

    Patient Details included

--- src/Patient.java
+++ src/Patient.java
@@ -1,9 +1,10 @@
 public class Patient extends Person{
 
-    private age;
-    private diseases;
-    private bloodgroup;
+    private int age;
+    private String diseases;
+    private String bloodgroup;
 
+    Patient(String name, int id, String phone_number, String address, int age, String diseases, )
 
 
 }
