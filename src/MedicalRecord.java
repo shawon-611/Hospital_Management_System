e3e78cbac7515e8aee2d7810e733fb5f37fd6b21
MD Alauddin Shawon <sa9881950@gmail.com>
2026-09-12 19:25:12 +0600

    Medical Record Updated

--- src/MedicalRecord.java
+++ src/MedicalRecord.java
@@ -1,3 +1,13 @@
 public class MedicalRecord {
 
+    private String record_id;
+    private String patient_id;
+    private String doctor_id;
+    private String diagnosis;
+    private String treatment;
+
+    MedicalRecord(String record_id, String patient_id, String doctor_id, String diagnosis, String treatment)
+    {
+
+    }
 }
