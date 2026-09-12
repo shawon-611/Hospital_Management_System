666331edd6f09d157b9b84bb6d1cb4987b08bae7
MD Alauddin Shawon <sa9881950@gmail.com>
2026-09-12 16:00:13 +0600

    Patient Details Modified

--- src/Patient.java
+++ src/Patient.java
@@ -1,20 +1,27 @@
 public class Patient extends Person{
 
     private int age;
+    private String address;
     private String diseases;
     private String blood_group;
 
-    Patient(String name, int id, String phone_number, String address, int age, String diseases, String blood_group)
+    Patient(String name, int id, String phone_number,int age, String address, String diseases, String blood_group)
     {
-        super(name, id, phone_number, address);
+        super(name, id, phone_number);
         this.age = age;
+        this.address = address;
         this.diseases = diseases;
         this.blood_group = blood_group;
     }
 
+
     public int getAge()
     {
         return age;
+    }
+    public String getAddress()
+    {
+        return address;
     }
     public String getDiseases()
     {
@@ -29,6 +36,10 @@ public class Patient extends Person{
     {
         this.age = age;
     }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
     public void setDiseases(String diseases)
     {
         this.diseases = diseases;
@@ -42,8 +53,11 @@ public class Patient extends Person{
     @Override
     public void displayInfo()
     {
-        super.displayInfo();
+        System.out.println("Patient Name: "+ getName());
+        System.out.println("Patient ID: "+ getID());
+        System.out.println("Patient Phone Number: "+ getPhone_Number());
         System.out.println("Patient Age: "+ age);
+        System.out.println("Patient Address: "+ address);
         System.out.println("Patient Diseases: "+ diseases);
         System.out.println("Patient Blood Group: "+ blood_group);
     }
