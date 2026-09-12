bc821045a9361a8c6064b83fb7dbbd03137991c9
MD Alauddin Shawon <sa9881950@gmail.com>
2026-09-12 19:25:52 +0600

    Hospital Management System Created

---
+++ src/HospitalManagementSystem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class HospitalManagementSystem {
+
+    private ArrayList<Patient> PatientList = new ArrayList<>();
+    private ArrayList<Doctor> DoctorList = new ArrayList<>();
+
+    private ArrayList<Appointment> AppointmentList = new ArrayList<>();
+
+
+
+
+}
