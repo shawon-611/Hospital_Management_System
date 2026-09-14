cf09e3d6359e3f678d0ad1e21a392142ae8b27e2
2024200000608-ai <2024200000640@seu.edu.bd>
2026-09-14 12:15:12 +0600

    added getter setter method in MedicalRecord

--- src/MedicalRecord.java
+++ src/MedicalRecord.java
@@ -8,6 +8,10 @@ public class MedicalRecord {
 
     MedicalRecord(String record_id, String patient_id, String doctor_id, String diagnosis, String treatment)
     {
-
+        this.record_id =  record_id;
+        this.patient_id = patient_id;
+        this.doctor_id = doctor_id;
+        this.diagnosis = diagnosis;
+        this.treatment = treatment;
     }
 }
