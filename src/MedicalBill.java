ed0c806181534e90c5fae1710bad4c321df14641
Codersabbir1000 <sabbirahmedshuvo420@gmail.com>
2026-09-18 20:33:56 +0600

    Medical Bill Updated

--- src/MedicalBill.java
+++ src/MedicalBill.java
@@ -1,3 +1,69 @@
 public class MedicalBill {
+    private int bill_id;
+    private int patient_id;
+    private double amount;
+    private String bill_date;
+    private String payment_status;
+
+    MedicalBill(int bill_id, int patient_id, double amount, String bill_date, String payment_status)
+    {
+        this.bill_id = bill_id;
+        this.patient_id = patient_id;
+        this.amount = amount;
+        this.bill_date = bill_date;
+        this.payment_status = payment_status;
+    }
+
+    public int getBill_id()
+    {
+        return bill_id;
+    }
+    public int getPatient_id()
+    {
+        return patient_id;
+    }
+    public double getAmount()
+    {
+        return amount;
+    }
+    public String getBill_date()
+    {
+        return bill_date;
+    }
+    public String getPayment_status()
+    {
+        return payment_status;
+    }
+
+    public void setBill_id(int bill_id)
+    {
+        this.bill_id = bill_id;
+    }
+    public void setPatient_id(int patient_id)
+    {
+        this.patient_id = patient_id;
+    }
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+    public void setBill_date(String bill_date)
+    {
+        this.bill_date = bill_date;
+    }
+    public void setPayment_status(String payment_status)
+    {
+        this.payment_status = payment_status;
+    }
+
+    public void displayInfo()
+    {
+        System.out.println("---Bill Details---\n");
+        System.out.println("Bill ID: "+ bill_id);
+        System.out.println("Patient ID: "+ patient_id);
+        System.out.println("Bill Amount: "+ bill_date);
+        System.out.println("Payment Status: "+ payment_status);
+        System.out.println(" ");
+    }
 
 }
