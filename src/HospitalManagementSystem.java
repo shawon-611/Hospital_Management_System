c134630d85259f76df54a5655a898e8a83089b6f
Codersabbir1000 <sabbirahmedshuvo420@gmail.com>
2026-09-24 17:50:30 +0600

    Medical Bill File load added

--- src/HospitalManagementSystem.java
+++ src/HospitalManagementSystem.java
@@ -14,6 +14,7 @@ public class HospitalManagementSystem {
         fileManager.loadPatientFile();
         fileManager.loadDoctorFile();
         fileManager.loadAppointmentFile();
+        fileManager.loadMedicalBillFile();
     }
 
     public ArrayList<Patient> getPatientList() {
